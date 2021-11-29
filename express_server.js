dcae454c78ed6d993bf21a5a12020bf6d3b50276
Matthew Tran <m.tran906@gmail.com>
2021-11-29 19:54:07 +0000

    Login/Logout + HTML bootstrap

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express(); 
 const PORT = 8080; 
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 app.set("view engine", "ejs"); 
 
 let urlDatabase = {
@@ -24,17 +25,22 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};
+  const templateVars = { username: req.cookies["username"], shortURL: shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
 
@@ -68,6 +74,28 @@ function generateRandomString(len) {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls')
+});
+
+
+app.get('/login', function (req, res) {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  console.log(templateVars);
+  res.render("urls_index", templateVars);
+})
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/urls')
 });
 
 app.listen(PORT, () => {
